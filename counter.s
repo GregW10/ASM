fa4979663f941c8b5cbfe199bb889f33df5d2f0e
El Macho Menos de C++ <mario.garwh@gmail.com>
2022-12-23 01:28:15 +0100

    Seg faults.

--- counter.s
+++ counter.s
@@ -9,10 +9,14 @@ _main:
     pushq %rbp
     movq %rsp, %rbp
 
-    movq $67, %rdi
-        callq _putchar
+    subq $40, %rsp
 
-    subq $8, %rsp
+    movq $67, %rdi
+    #callq _putchar
+
+    addq $40, %rsp
+
+    #subq $8, %rsp
     #addq $8, %rsp
 
 
@@ -23,17 +27,23 @@ while:
     leaq counter_str(%rip), %rdi
     movq (%rsp), %rsi
     xorq %rax, %rax
+    subq $40, %rsp
     callq _printf
+    addq $40, %rsp
     movq $1, %rdi
+    subq $40, %rsp
     callq _sleep
+    addq $40, %rsp
 
     decq 0(%rsp)
     jnz while
 
-    addq $8, %rsp
+    #addq $8, %rsp
 
     leaq done_str(%rip), %rdi
+    subq $40, %rsp
     callq _printf
+    subq $40, %rsp
 
     movq $0, %rax
 
